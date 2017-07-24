6f7f9146be68e050b235324dba03335593526773
Manraaj Nijjar <ManraajNijjar@gmail.com>
2017-07-24 14:43:04 -0400

    Adding in the Data Models

---
+++ Models/Photo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Photo+CoreDataClass.swift
+//  VirtualTourist
+//
+//  Created by Manraaj Nijjar on 7/24/17.
+//  Copyright © 2017 Manraaj Nijjar. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Photo: NSManagedObject {
+
+}
