4e8aaab8fcee6cccd45a04e17b9a796e3256744e
Manraaj Nijjar <ManraajNijjar@gmail.com>
2017-07-30 16:45:58 -0400

    Set up DataSource and Delegate for CollectionView as well as Programmatic cell size

--- PinViewController.swift
+++ PinViewController.swift
@@ -15,17 +15,34 @@ class PinViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
     var pinForPinView: Pin!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         mapSetup()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        adjustFlowLayout(viewSize: view.frame.size)
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        adjustFlowLayout(viewSize: size)
+    }
+    
+    func adjustFlowLayout(viewSize: CGSize) {
+        let space:CGFloat = 3.0
+        let dimension: CGFloat = viewSize.width >= viewSize.height ? (viewSize.width - (5 * space)) / 6.0 : (viewSize.width - (2 * space)) / 3.0
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
     }
     
     //Sets up the map view properly. Called when the view loads
@@ -38,5 +55,23 @@ class PinViewController: UIViewController {
         annotation.coordinate = mapCenter
         mapView.addAnnotation(annotation)
     }
+    
+}
+
+extension PinViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 25
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as! PhotoCollectionViewCell
+        
+        cell.label.text = String(indexPath.row)
+        
+        return cell
+    }
+    
     
 }
