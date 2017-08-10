b30eb4fd817d0ec00f5e1180837586cf4965d627
Manraaj Nijjar <manraajnijjar@gmail.com>
2017-08-10 19:11:55 -0400

    Add files via upload

---
+++ VirtualTourist/Pin+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Pin+CoreDataClass.swift
+//  VirtualTourist
+//
+//  Created by Manraaj Nijjar on 7/31/17.
+//  Copyright © 2017 Manraaj Nijjar. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Pin: NSManagedObject {
+
+}
---
+++ VirtualTourist/VirtualTourist/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  VirtualTourist
+//
+//  Created by Manraaj Nijjar on 7/23/17.
+//  Copyright © 2017 Manraaj Nijjar. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
